64a1f9cd6cf6e5e4887efae59f583368edd7bedf
edU <131560672+Cad-prof@users.noreply.github.com>
2025-04-08 11:51:22 +0000

    mongoDB connect = done :)

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
-// const express = require("express");
+// const express = require("express");   
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) =>{
-  res.send
+app.get("/products", (req, res) =>{
+  res.send("Server is ready!");
 })
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  connectDB();
+  console.log("\n Server is running on port http://localhost:3000/");
 });
+
+// usxBoZpFgBoJfwll
